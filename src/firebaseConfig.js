80584e11066020627f890f923c4480629c5f14cd
onlinecalculatorhub <hubonlinecalculator@gmail.com>
2025-02-09 10:42:57 +0530

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBliXjYaDB8vwICouYKplg4k2fxQo92QJs",
+    authDomain: "document-59e41.firebaseapp.com",
+    projectId: "document-59e41"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
